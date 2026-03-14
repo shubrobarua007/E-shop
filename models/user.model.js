f31aceb2a3c38acfe962d9aa55db8045bf41b900
Your Name <youremail@example.com>
2026-03-14 07:22:56 +0400

    create usermodel

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required "],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
